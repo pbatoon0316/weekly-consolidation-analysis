9933c33303510dc9b465718440a8af609f3517d4
Patrick Batoon <118654860+pbatoon0316@users.noreply.github.com>
2024-10-11 12:35:42 -0700

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import yfinance as yf
-from finta import TA
-import math
 import os
+import time
 
-os.chdir(r'C:\Users\pbato\OneDrive\Documents\Python-Streamlit\weekly-consolidation-analysis-main')
+os.chdir(r'C:\Users\pabatoon\Downloads\weekly-consolidation-analysis-main')
 print("Current working directory:", os.getcwd())
 
 #%% Obtain tickers from raw nasdaq table. With data cleanup
@@ -12,7 +11,7 @@ def clean_metadata(metadata_csv):
     metadata = pd.read_csv(metadata_csv)
     metadata.dropna(subset=['Market Cap'], inplace=True)
     metadata = metadata.sort_values(by='Market Cap', ascending=False)
-    metadata['Symbol'] = metadata['Symbol'].str.replace('/', '.')
+    metadata['Symbol'] = metadata['Symbol'].str.replace('/', '-')
     metadata.reset_index(drop=True, inplace=True)
     return metadata 
 
@@ -20,18 +19,12 @@ def get_tickers(metadata, minval=0, maxval=2000):
     tickers = metadata['Symbol'][minval:maxval].tolist()
     return tickers
 
-metadata_csv = 'nasdaq_screener_1727589550419.csv'
-metadata = clean_metadata(metadata_csv)
-tickers =  get_tickers(metadata, minval=0, maxval=2000) ###['WBD','CMCSA','PBR','CNH','LUV']
-
 
 #%% Download stock data from yfinance
 
 def download_data_wk(tickers):
-    data = yf.download(tickers, period='6mo', interval='1wk', auto_adjust=True, progress=True)
+    data = yf.download(tickers, period='1y', interval='1wk', auto_adjust=True, progress=True)
     return data
-
-data_wk = download_data_wk(tickers)
 
 #%% Input stock data into squeeze screener
 
@@ -40,7 +33,6 @@ def scanner_wk(data):
     squeezes = pd.DataFrame()
 
     for ticker in tickers:
-        print(ticker)
         df = data.loc[:, (slice(None), ticker)].copy()
         
         df.dropna(inplace=True)
@@ -54,18 +46,26 @@ def scanner_wk(data):
         # Volume
         df['volume_average'] = df['volume'].mean()
         df['volume_zscore'] = (df['volume'] - df['volume'].shift(1).mean()) / df['volume'].shift(1).std()
-        # KC
-        KC = TA.KC(df, period=20, atr_period=20, kc_mult=1.5)
-        # BB
-        BB = TA.BBANDS(df, period=20, std_multiplier=2)
-        # AO
-        AO = TA.AO(df, slow_period=20, fast_period=10)
-        # Concatenate
-        df = pd.concat([df, KC, BB, AO], axis=1)
+        
+        #Exponential Moving Averages
+        df['EMA50'] = df['close'].ewm(50).mean()
+        df['EMA20'] = df['close'].ewm(20).mean()
+        df['EMA10'] = df['close'].ewm(10).mean()
+        
+        # Keltner Channel
+        df['ATR'] = (df['high'] - df['low']).rolling(20).mean()
+        df['KC_UPPER'] = df['EMA20'] + 1.5*df['ATR']
+        df['KC_LOWER'] = df['EMA20'] - 1.5*df['ATR']
+        # Bollinger Bands
+        df['BB_UPPER'] = df['EMA20'] + 2*df['close'].rolling(20).std()
+        df['BB_LOWER'] = df['EMA20'] - 2*df['close'].rolling(20).std()
+        
+        # Awesome Oscillator
+        df['AO'] =  df['EMA10'] - df['EMA20']
 
         # Conditions
         df['SQUEEZE'] = (df.BB_LOWER >= df.KC_LOWER) | (df.BB_UPPER <= df.KC_UPPER)
-        df['ASCENDING'] = (df.AO.iloc[-1] > df.AO.iloc[-2]) & (df.AO.iloc[-2] > df.AO.iloc[-3]) & (df.AO.iloc[-3] > df.AO.iloc[-4])
+        df['ASCENDING'] = (df.close.iloc[-1] > df.EMA20.iloc[-1]) & (df.AO.iloc[-1] > df.AO.iloc[-2]) #& (df.AO.iloc[-2] > df.AO.iloc[-3]) & (df.AO.iloc[-3] > df.AO.iloc[-4])
 
         df.dropna(inplace=True)
 
@@ -78,4 +78,16 @@ def scanner_wk(data):
 
     return squeezes
 
+
+#%%
+metadata_csv = 'nasdaq_screener_1727589550419.csv'
+metadata = clean_metadata(metadata_csv)
+tickers = get_tickers(metadata, minval=0, maxval=2000) 
+#tickers = ['WBD','CMCSA','PBR','CNH','LUV']
+data_wk = download_data_wk(tickers)
+
+#%%
+start = time.time()
 sqeezes_wk = scanner_wk(data_wk)
+stop = time.time()
+print(f'{stop-start} sec')
